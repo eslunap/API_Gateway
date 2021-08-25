99215fd95429cd9e2039438a8b0bb0bd00fe6a62
eslunap19 <estefany.luna@gmail.com>
2021-08-25 03:58:11 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 module.exports = {
     users_api_url : process.env.USERS_API_URL || 'https://libraryapp-mintic-users.herokuapp.com/',
-    books_api_url: process.env.BOOKS_API_URL || 'https://libreria-mision-tic.herokuapp.com/editorial',
+    books_api_url: process.env.BOOKS_API_URL || 'https://libreria-mision-tic.herokuapp.com/',
+    orders_api_url: process.env.ORDERS_API_URL || 'https://libraryapp-ordenes.herokuapp.com/',
 };
