0ee02620acb4097655aa64f423464a6946f7a263
eslunap19 <estefany.luna@gmail.com>
2021-08-25 01:57:40 -0500

    URL's added to server.js
    se agregan las url de los ms desplegados en heroku

---
+++ src/server.js
@@ -0,0 +1,4 @@
+module.exports = {
+    users_api_url : process.env.USERS_API_URL || 'https://usuariosmselp.herokuapp.com/',
+    accounts_api_url: process.env.ACCOUNTS_API_URL || 'https://cuentasmselp.herokuapp.com/',
+};
