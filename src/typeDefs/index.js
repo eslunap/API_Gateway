5b21917f85c58662f604dc640be808a349cc5ce9
Vanessa V <vanevb976@gmail.com>
2021-08-28 14:58:19 -0500

    Index_typedefs

---
+++ src/typeDefs/index.js
@@ -0,0 +1,21 @@
+const librosTypeDefs = require ('./librosTypeDef');
+const authorTypeDefs = require ('./authorTypeDef');
+const categoriasTypeDefs = require ('./categoriasTypeDef');
+const editorialTypeDefs = require ('./editorialsTypeDef');
+const productoImgTypeDefs = require ('./productoImgTypeDef');
+
+const schemasArray = [
+
+    authorTypeDefs,
+    categoriasTypeDefs,
+    editorialTypeDefs,
+    librosTypeDefs,
+    productoImgTypeDefs,
+];
+
+module.exports = schemasArray;
+ 
+
+
+
+
