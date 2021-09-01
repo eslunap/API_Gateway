9e008285c5b3e5844a7a6c3ac5251ec1f23ed8dc
eslunap19 <estefany.luna@gmail.com>
2021-08-31 23:08:33 -0500

    Actualizacion_urls
    Cambio de url's por nuevos despliegues

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 module.exports = {
-    users_api_url : process.env.USERS_API_URL || 'https://libraryapp-mintic-users.herokuapp.com/',
-    books_api_url: process.env.BOOKS_API_URL || 'https://libreria-mision-tic.herokuapp.com/',
-    orders_api_url: process.env.ORDERS_API_URL || 'https://libraryapp-ordenes.herokuapp.com/',
+    users_api_url : process.env.USERS_API_URL || 'https://libreriausers.herokuapp.com/',
+    books_api_url: process.env.BOOKS_API_URL || 'https://libreriamain.herokuapp.com/',
+    orders_api_url: process.env.ORDERS_API_URL || 'https://libreriaordenes.herokuapp.com/',
 };
